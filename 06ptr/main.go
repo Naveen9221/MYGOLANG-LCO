a27d5071ae35c86147bc1c8faf70588fc22e4d2e
Naveen <naveennaveen9221@gmail.com>
2022-11-26 18:55:20 +0530

    new file

--- 06ptr/main.go
+++ 06ptr/main.go
@@ -9,4 +9,11 @@ func main() {
 	fmt.Println(mynumber)
 	fmt.Println(ptr)
 	fmt.Println(*ptr + 2)
+
+	//arrays
+
+	var list [4]string // or  var list=[4] string{"123","345","56","5"}
+	list[0] = "123"
+	list[3] = "456"
+	fmt.Println(list, "length", len(list))
 }
